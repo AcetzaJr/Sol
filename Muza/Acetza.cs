c8436f012f35b4b480383c27dce57af3d6ef830e
José Martínez <marquevjosman@gmail.com>
2024-06-18 21:16:36 -0600

    up

--- Muza/Acetza.cs
+++ Muza/Acetza.cs
@@ -5,17 +5,17 @@ namespace Muza
         private static readonly float[] rations =
         [
             (float)1 / 1,
-        (float)256 / 243,
-        (float)9 / 8,
-        (float)32 / 27,
-        (float)81 / 64,
-        (float)4 / 3,
-        Tempered.Frequency(6, baseFrequency: 1.0f),
-        (float)3 / 2,
-        (float)128 / 81,
-        (float)27 / 16,
-        (float)16 / 9,
-        (float)243 / 128
+            (float)256 / 243,
+            (float)9 / 8,
+            (float)32 / 27,
+            (float)81 / 64,
+            (float)4 / 3,
+            Tempered.Frequency(6, baseFrequency: 1.0f),
+            (float)3 / 2,
+            (float)128 / 81,
+            (float)27 / 16,
+            (float)16 / 9,
+            (float)243 / 128
         ];
         public static float baseFrequency = Constants.BaseFrequency;
         public static float Ration(int note)
@@ -24,7 +24,7 @@ namespace Muza
         }
         public static float Power(int note)
         {
-            return MathF.Pow(2.0f, note < note - 12 ? 0 : note / 12);
+            return MathF.Pow(2.0f, note < 0 ? ((note + 1) / 12) - 1 : note / 12);
         }
         public static float Frequency(int note)
         {
